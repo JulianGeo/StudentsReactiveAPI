78dadb1ef8ba10a62cb3e473cab7796f753f722e
Julian Rojas <julianrojasgeo@gmail.com>
2023-03-22 16:17:21 -0500

    save student use case test

--- src/test/java/com/reactiveAPP/studentsAPI/usecases/SaveStudentUseCaseTest.java
+++ src/test/java/com/reactiveAPP/studentsAPI/usecases/SaveStudentUseCaseTest.java
@@ -1,11 +1,19 @@
 package com.reactiveAPP.studentsAPI.usecases;
 
+import com.reactiveAPP.studentsAPI.domain.collection.Student;
+import com.reactiveAPP.studentsAPI.domain.dto.StudentDTO;
 import com.reactiveAPP.studentsAPI.repository.StudentRepository;
+import com.reactiveAPP.studentsAPI.util.InstanceProvider;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,5 +30,27 @@ class SaveStudentUseCaseTest {
         mapper = new ModelMapper();
         saveStudentUseCase = new SaveStudentUseCase(repoMock, mapper);
     }
+
+    @Test
+    @DisplayName("saveCourse_Success")
+    void SaveCourse(){
+        var studentDTO =mapper.map(InstanceProvider.getStudent(), StudentDTO.class);
+
+        Mockito.when(repoMock.save(mapper.map(studentDTO, Student.class)))
+                .thenAnswer(invocationOnMock -> Mono.just(invocationOnMock.getArgument(0)));
+
+        var service = saveStudentUseCase.apply(studentDTO);
+
+        StepVerifier.create(service)
+                //Test to check next object as a whole
+                .expectNext(mapper.map(InstanceProvider.getStudents().get(0), StudentDTO.class))
+                //Custom and specific test for the expected object
+                //.expectNextMatches(studentDTO1 -> studentDTO1.getName().equals("Wout"))
+                //Test to check the size of the stream
+                //.expectNextCount(1)
+                .verifyComplete();
+        Mockito.verify(repoMock).save(mapper.map(studentDTO, Student.class));
+    }
+
 
 }
